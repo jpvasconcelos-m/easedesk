66238049d3412caeec06a9c6ac1b73203cbbe9f4
joao.mendes <joaopedrosao@gmail.com>
2023-10-02 01:43:39 -0300

    criado usuarios

--- src/main/java/sistemachamados/model/Chamado.java
+++ src/main/java/sistemachamados/model/Chamado.java
@@ -1,14 +1,23 @@
 package sistemachamados.model;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Random;
 
 
 public class Chamado {
 
+    Integer numSolicitacao;
+    String Contrato;
+    Instant dataCriacao;
+    Instant prazoLimite;
+    Usuario usuarioResponsavel;
+
+
 
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt()*10000);
+
     }
+
 
 
 
