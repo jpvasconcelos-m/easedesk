f9155cb7452a89d144069fafe2d009e7202152b4
joao.mendes <joaopedrosao@gmail.com>
2023-10-02 01:44:00 -0300

    criado teste para chamado

---
+++ src/test/java/testeChamado.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sistemachamados.model.Chamado;
+
+public class testeChamado {
+
+    private Chamado chamado;
+
+    @BeforeEach
+    void iniciar(){
+
+
+    }
+    @Test
+    void testeInstancia(){
+        chamado.equals(2);
+    }
+
+
+
+}
