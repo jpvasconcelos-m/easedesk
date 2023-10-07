65b3793181f131ffd537274db6787ce7f0b42c85
joaop <joaopedrosao@gmail.com>
2023-10-07 14:39:54 -0300

    Adicionados novos testes para FilaChamados

--- src/test/java/TesteFilaChamados.java
+++ src/test/java/TesteFilaChamados.java
@@ -1,2 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sistemachamados.model.Chamado;
+import sistemachamados.model.FilaChamados;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sistemachamados.model.Chamado;
+import sistemachamados.utils.Utilitarios;
+import org.junit.jupiter.api.Assertions.*;
+
+import java.time.Instant;
+
+
+
 public class TesteFilaChamados {
+
+    private  Chamado chamado;
+    private  FilaChamados filaChamados;
+
+
+
+
+
+
+    @Test
+    void testeAdicionarAFila() {
+        FilaChamados filaChamados = new FilaChamados();
+        Chamado chamado1 = new Chamado(4321,"ses", Instant.now(),Instant.now(),"Aberto",1,"Impressora explodiu");
+        Chamado chamado2 = new Chamado(1234,"see", Instant.now(),Instant.now(),"Aberto",1,"Impressora pipocou ao vivo");
+
+
+        Assertions.assertTrue(filaChamados.adicionarAFila(chamado1));
+        Assertions.assertTrue(filaChamados.adicionarAFila(chamado2));
+        filaChamados.chamados.stream().forEach(System.out::println);
+
+    }
+
 }
