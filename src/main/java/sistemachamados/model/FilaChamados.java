753be92707f6de5862bb4763cbe905cfb8e9db44
joaop <joaopedrosao@gmail.com>
2023-10-07 15:15:12 -0300

    Criado método para adicionar chamado a fila de chamados.

--- src/main/java/sistemachamados/model/FilaChamados.java
+++ src/main/java/sistemachamados/model/FilaChamados.java
@@ -1,10 +1,35 @@
 package sistemachamados.model;
 
+import java.time.Instant;
 import java.util.LinkedList;
 
  public class FilaChamados {
 
-    LinkedList<Chamado> chamados = new LinkedList<>();
+    public LinkedList<Chamado> chamados = new LinkedList<>();
+
+
+    public  boolean adicionarAFila(Chamado chamado){
+
+        if(chamados.add(chamado) == true){
+            System.out.println("Chamado adicionado!");
+            return true;
+        }
+       else if(chamados.add(chamado) == false){
+            System.out.println("Chamado falhou em ser adicionado :/");
+            return false;
+        }
+       else
+       return false;
+
+
+    }
+
+
+
+
+
+
+
 
 
 
