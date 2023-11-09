da61d67b9a6f5e14eb581fcda254555f8f6c9cac
joao.mendes <joaopedrosao@gmail.com>
2023-11-09 02:11:33 -0300

    adicionado metodo finalizarchamado alteração no scopo do scanner.

--- src/main/java/sistemachamados/model/UsuarioTI.java
+++ src/main/java/sistemachamados/model/UsuarioTI.java
@@ -6,6 +6,8 @@ import static sistemachamados.model.FilaChamados.fila;
 
 public class UsuarioTI extends UsuarioComum {
 
+   static  Scanner sc= new Scanner(System.in);
+
 
 
 
@@ -14,23 +16,41 @@ public class UsuarioTI extends UsuarioComum {
     }
 
     public void capturarChamado(){
-        Scanner sc= new Scanner(System.in);
+
+        Menu.verFila();
+
         System.out.println("Digite o índice do chamado que deseja capturar: ");
         int indice =  sc.nextInt();
         Chamado chamado = fila.get(indice);
         chamado.setUsuarioResponsavel(this);
+        chamado.setStatus(Status.CAPTURADO);
 
-        System.out.printf("\nO usuario responsável pelo chamado '%s' é : %s",chamado.getDescricao(),chamado.getUsuarioResponsavel().getLogin());
-
-
-
-
-
+        System.out.printf("\nO usuario responsável pelo chamado ID: %d, DESCRIÇÃO: '%s' é : %s\n",chamado.id,chamado.getDescricao(),chamado.getUsuarioResponsavel().getLogin());
+        Menu.mostrarOpcoes();
 
 
 
     }
 
+    public  void finalizarChamado(){
+
+        System.out.println("Escolha o ID de um chamado que deseja finalizar: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+
+        System.out.println("Digite uma solução: ");
+        String solucao = sc.nextLine();
+        fila.get(id).setSolucao(solucao);
+        fila.get(id).setStatus(Status.CONCLUIDO);
+        Menu.mostrarOpcoes();
+
+
+    }
+
+    public void consultarChamadosCapturadosPorMim(){
+        System.out.println((fila.stream().filter(chamado -> chamado.usuarioResponsavel == this && chamado.status == Status.CAPTURADO)).toString());
+    }
 
 
 
