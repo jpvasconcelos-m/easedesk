33d45142c87c6929be183e8bf8212f425fe90e6b
joao.mendes <joaopedrosao@gmail.com>
2023-12-22 21:10:57 -0300

    created test for check user adding to postgres.

---
+++ src/main/java/easedesk/test/UserCreator.java
@@ -0,0 +1,27 @@
+package easedesk.test;
+
+import easedesk.infra.DAO;
+import easedesk.model.User;
+
+
+public class UserCreator {
+
+    public static void main(String[] args) {
+        User user1 = new User("Renato Trajano Melo","renatotrajanom@gmail.com",false);
+        User user2 = new User("Luis Fernandes Alcântara","lfa@gmail.com",false);
+        User user3 = new User("Joelma Tamires Dionísio","joelmadionisio97@gmail.com",false);
+
+        DAO<User> dao = new DAO<>(User.class);
+        dao.openTransaction();
+        dao.include(user1);
+        dao.include(user2);
+        dao.include(user3);
+        dao.closeTransaction();
+
+        System.out.println("User1 ID:" + user1.getId());
+        System.out.println("User2 ID:" + user2.getId());
+    }
+
+
+
+}
