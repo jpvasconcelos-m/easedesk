9edc3473d1b74e76df9e746706ecb66c13295cde
jpv <joaopedrosao@gmail.com>
2023-12-25 21:11:25 -0300

    Created main method class. Some TODOS proposed.

---
+++ src/main/java/easedesk/model/Easedesk.java
@@ -0,0 +1,14 @@
+package easedesk.model;
+
+
+//Main class!
+//TODO Implement Spring boot framework!
+
+
+
+public class Easedesk {
+    public static void main(String[] args) {
+        System.out.println("Menu:");
+        System.out.println();//TODO create method for each function using other class from model.
+    }
+}
