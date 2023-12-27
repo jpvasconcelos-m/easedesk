caaab9aed18ba84f7220aa00fb70553048feb058
joao.mendes <joaopedrosao@gmail.com>
2023-12-27 00:19:42 -0300

    added showMenu() and register() methods

--- src/main/java/easedesk/model/Easedesk.java
+++ src/main/java/easedesk/model/Easedesk.java
@@ -5,10 +5,36 @@ package easedesk.model;
 //TODO Implement Spring boot framework!
 
 
+import easedesk.infra.DAO;
 
 public class Easedesk {
     public static void main(String[] args) {
-        System.out.println("Menu:");
-        System.out.println();//TODO create method for each function using other class from model.
+        //TODO create method for each function using other class from model.
+        showMenu();
+
+
     }
+
+    static void showMenu(){
+        System.out.println("Menu:");
+        System.out.println(" 1 - Login");
+        System.out.println(" 2 - Forgot my credentials");
+        System.out.println(" 3 - Register");
+    }
+    static void register(String name, String email, boolean isAdmin){
+        //TODO create later overwrite with an method with friendly user Interface, not CLI.
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
