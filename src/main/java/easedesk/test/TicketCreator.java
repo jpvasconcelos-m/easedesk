53c6f84d3addbde6f9e9cc2b684355600c5e21a6
joao.mendes <joaopedrosao@gmail.com>
2023-12-29 00:11:49 -0300

    Created class for testing Tickets

---
+++ src/main/java/easedesk/test/TicketCreator.java
@@ -0,0 +1,23 @@
+package easedesk.test;
+
+import easedesk.infra.DAO;
+import easedesk.model.Ticket;
+import easedesk.model.User;
+
+public class TicketCreator {
+    public static void main(String[] args) {
+        DAO<User> userDAO = new DAO<>(User.class);
+
+        User user = userDAO.obtainById(9L);
+        System.out.println(user.getTicket());
+        userDAO.close();
+
+        DAO<Ticket> ticketDAO = new DAO<>(Ticket.class);
+        Ticket ticket = new Ticket("Printer exploded", user);
+        ticketDAO.openTransaction();
+        ticketDAO.include(ticket);
+        ticketDAO.closeTransaction();
+
+    }
+
+}
