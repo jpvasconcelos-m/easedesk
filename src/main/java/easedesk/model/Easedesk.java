9969b31aa4a7b6f21e69135b73f1c011c1da7938
joao.mendes <joaopedrosao@gmail.com>
2024-01-01 21:20:50 -0300

    added addtoDB method and update register method.

--- src/main/java/easedesk/model/Easedesk.java
+++ src/main/java/easedesk/model/Easedesk.java
@@ -8,12 +8,15 @@ package easedesk.model;
 import easedesk.infra.DAO;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Easedesk {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         //TODO create method for each function using other class from model.
         showMenu();
-        search();
+        addToDB(new User("João Pedro Vasconcelos Mendes","joaopedrosao@gmail.com",true));
+        register();
 
 
 
@@ -26,8 +29,43 @@ public class Easedesk {
         System.out.println(" 2 - Forgot my credentials");
         System.out.println(" 3 - Register");
     }
-    static void register(User user){
-        //TODO create later overwrite with an method with friendly user Interface, not CLI.
+
+    static void register(){
+        while(true) {
+            try {
+                System.out.println("Inform your name");
+                String name = sc.nextLine();
+                System.out.println("Inform your email :");
+                String email = sc.nextLine();
+                System.out.println("You're admin? (Y/N)");
+                Object isAdmin = sc.nextLine();
+                if (isAdmin.toString().equalsIgnoreCase("YES") || (isAdmin.toString().equalsIgnoreCase("Y"))) {
+                    isAdmin = true;
+                    System.out.println(isAdmin);
+                } else if (isAdmin.toString().equalsIgnoreCase("NO") || (isAdmin.toString().equalsIgnoreCase("N"))) {
+                    isAdmin = false;
+                    System.out.println(isAdmin);
+                } else
+                    System.out.println(isAdmin);
+
+                addToDB(new User(name, email, (boolean) isAdmin));
+                break;
+            }
+            catch (ClassCastException exception){
+                System.out.println("Invalid value! Try again!");
+
+            }
+        }
+
+
+    }
+    static void addToDB(User user){
+
+        DAO<User> dao = new DAO<>(User.class);
+
+        dao.openTransaction();
+        dao.include(user);
+        dao.closeTransaction();
 
        }
     static void search(){
